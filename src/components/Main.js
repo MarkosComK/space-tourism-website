d17222987ece34c34bcfbce37e995c1b589ef71c
Markos Vinicius <markoscomks@gmail.com>
2021-10-22 08:24:53 -0300

    Setup main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,18 @@ import '../css/main.css'
 function Main() {
     return (
         <div className="main-container">
-            
+            <main>
+                <div className="text-container">
+                    <h5>So, you want to travel to</h5>
+                    <h1>Space</h1>
+                    <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                </div>
+                <a>
+                    <div className="explore-button">
+                        <p>explore</p>
+                    </div>
+                </a>
+            </main>
         </div>
     )
 }
