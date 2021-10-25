34462704dd935ec253cf789836568c3715c48b18
Markos Vinicius <markoscomks@gmail.com>
2021-10-25 14:44:24 -0300

    Navbar responsivo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,40 @@
 import React from 'react'
+import { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import '../css/navbar.css'
 
-function Navbar() {
-    return (
-        <div>
-            <header>
-                <nav className="app-header">
-                    <div className="icon"></div>
-                    <div className="line"></div>
-                    <div className="links-container">
-                        <div className="ul-container">
-                            <ul>
-                                <li><Link to='/'><span>00</span> Home</Link></li>
-                                <li><Link to='/destination'><span>01</span> Destination</Link></li>
-                                <li><Link to='/crew'><span>02</span> Crew</Link></li>
-                                <li><Link to='/tecnology'><span>03</span> Tecnology</Link></li>
-                            </ul>
+class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className="app-header">
+                        <div className="icon"></div>
+                        <div className="line"></div>
+                        <div className={this.state.clicked ? "links-container": "links-container links-container-active"}>
+                            <div className="ul-container">
+                                <ul>
+                                    <li><Link to='/' onClick={this.handleClick}><span>00</span> Home</Link></li>
+                                    <li><Link to='/destination' onClick={this.handleClick}><span>01</span> Destination</Link></li>
+                                    <li><Link to='/crew' onClick={this.handleClick}><span>02</span> Crew</Link></li>
+                                    <li><Link to='/tecnology' onClick={this.handleClick}><span>03</span> Tecnology</Link></li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </nav>
-            </header>
-        </div>
-    )
+                        <div onClick={this.handleClick} className={this.state.clicked ? "svg-x" :"svg-hamburguer" }>
+                        </div>
+                    </nav>
+                </header>
+            </div>
+        )
+    }
 }
 
 export default Navbar
--- src/components/Tecnology.js
+++ src/components/Tecnology.js
@@ -1,7 +1,10 @@
 import React, {useState} from 'react'
 import launchImg from '../assets/technology/image-launch-vehicle-portrait.jpg'
+import launchImgResponsive from '../assets/technology/image-launch-vehicle-landscape.jpg'
 import spaceportImg from '../assets/technology/image-spaceport-portrait.jpg'
+import spaceportImgResponsive from '../assets/technology/image-spaceport-landscape.jpg'
 import capsuleImg from '../assets/technology/image-space-capsule-portrait.jpg'
+import capsuleImgResponsive from '../assets/technology/image-space-capsule-landscape.jpg'
 import '../css/tecnology.css'
 
 
@@ -9,16 +12,19 @@ function Tecnology() {
     const vehicle = {
         color: '',
         img: launchImg,
+        imgResponsive: launchImgResponsive,
         h4: 'Launch Vehicle',
         p: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
     }
     const spaceport = {
         img: spaceportImg,
+        imgResponsive: spaceportImgResponsive,
         h4: 'Spaceport',
         p: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
     }
     const capsule = {
         img: capsuleImg,
+        imgResponsive: capsuleImgResponsive,
         h4: 'Space Capsule',
         p: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
     }
@@ -51,8 +57,11 @@ function Tecnology() {
                 <div className="sec3-content-wrapper">
                     <div>
                         <div className="sec3-text-content">
-                            <h2><span>03</span> Space Launch 101</h2>
+                             <h2><span>03</span> Space Launch 101</h2>
                             <div className="sec3-bullets-text">
+                                <div className="sec3-img-responsive">
+                                    <img src={content.imgResponsive} alt="" />
+                                </div>
                                 <div className="sec3-bullets-nav">
                                     <div className="sec3-bullet1" onClick={setVehicle}>1</div>
                                     <div className="sec3-bullet2" onClick={setSpaceport}>2</div>
